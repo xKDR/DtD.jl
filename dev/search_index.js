e7efb2b1d140aac71da491f60315b6a7a34444f2
Documenter.jl <documenter@juliadocs.github.io>
2022-04-22 08:29:33 +0000

    build based on 24ea892

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DtD","category":"page"},{"location":"#DtD","page":"Home","title":"DtD","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DtD.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DtD]","category":"page"}]
+}
